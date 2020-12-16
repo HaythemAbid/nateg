aea6be4d814e1feca01951732e9d3159d18c0383
Haythem <abidhay2@gmail.com>
2020-12-16 20:42:58 +0100

    third activity

--- app/src/main/java/com/example/nategandroid/Second.java
+++ app/src/main/java/com/example/nategandroid/Second.java
@@ -2,11 +2,15 @@ package com.example.nategandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Second extends AppCompatActivity {
     private ImageView photo;
+    Button btn2 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,13 @@ public class Second extends AppCompatActivity {
 
         photo= findViewById(R.id.imageView);
         photo.setImageResource(R.drawable.download);
+        btn2= findViewById(R.id.button);
 
 
+    }
+    public void tonext(View view) {
+
+        Intent myintent2 = new Intent(Second.this , third.class);
+        Second.this.startActivity(myintent2);
     }
 }
